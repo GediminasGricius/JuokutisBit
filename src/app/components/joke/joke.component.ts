63510375df5629f58ab1f37fdb9a6186748a6d5a
GediminasGricius <g.gricius@ituostas.lt>
2022-06-09 21:29:55 +0300

    Loading, Error

--- src/app/components/joke/joke.component.ts
+++ src/app/components/joke/joke.component.ts
@@ -11,12 +11,22 @@ import { JokeService } from 'src/app/services/joke.service';
 export class JokeComponent implements OnInit {
 
   public joke:Joke|null=null;
+  public loading=true;
+  public error=false;
 
   constructor( private jokeService:JokeService) { }
 
   private loadJoke(){
-    this.jokeService.getJoke().subscribe((result)=>{
-      this.joke=result;
+    this.loading=true;
+    this.jokeService.getJoke().subscribe({
+      next:(result)=>{
+        this.joke=result;
+        this.loading=false;
+      },
+      error:(error)=>{
+        console.log(error.status);
+        this.error=true; 
+      }
     });
   }
 
